cbcad8b9c9bde15c1a0acabfa1ec57c670c6e8e4
DodgyHippy <brandonjones6@hotmail.com>
2026-05-31 23:25:12 +1000

    Set up main

--- src/GroupProject/Main.java
+++ src/GroupProject/Main.java
@@ -1,25 +1,58 @@
 package GroupProject;
 
 import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Location> gameMap;
-
-        gameMap = Init.mapGen();
+        ArrayList<Location> gameMap = Init.mapGen();
 
         Player player = Init.createPlayer(gameMap);
         Monster monster = Init.createMonster(gameMap);
-        Location currentTile = gameMap.get(player.getPosition());
 
-        monster.moveRandomly(gameMap);
-        if (monster.getPosition() == player.getPosition()) {
-            System.out.println("The monster has found you!");
-            Battle.startBattle(player, monster);
+        int totalSurveyDataNeeded = 2;
+        boolean gameRunning = true;
+
+        UI.introSequence();
+
+        while (gameRunning && player.isAlive()) {
+            Location currentTile = gameMap.get(player.getPosition());
+
+            UI.commandOptions(player, gameMap);
+
+            currentTile = gameMap.get(player.getPosition());
+
+            int surveyDataCollected = countSurveyedSites(gameMap);
+
+            if (surveyDataCollected == totalSurveyDataNeeded && currentTile.getHomeBase()) {
+                System.out.println("You returned to home base with all survey data. Mission complete!");
+                gameRunning = false;
+            }
+
+            if (gameRunning && player.isAlive()) {
+                monster.moveRandomly(gameMap);
+
+                if (monster.getPosition() == player.getPosition()) {
+                    System.out.println("The monster has found you!");
+                    Battle.startBattle(player, monster);
+                }
+            }
         }
 
-        //currentTile.getDeposit();
-        //System.out.println(currentTile.getHomeBase());
-        //Battle.main();
+        if (!player.isAlive()) {
+            System.out.println("You have died. Game over.");
+        }
+    }
+
+    public static int countSurveyedSites(ArrayList<Location> gameMap) {
+        int count = 0;
+
+        for (Location location : gameMap) {
+            if (location.getSurveySite() && location.isSiteSurveyed()) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
