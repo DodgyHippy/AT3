b125abccee5e96138c1876378b3654ca39eedd7a
Sebastion Wolkowski <s4245220@student.rmit.edu.au>
2026-05-31 22:21:13 +1000

    ItemType.java

--- src/GroupProject/ItemType.java
+++ src/GroupProject/ItemType.java
@@ -5,6 +5,11 @@ Author: Sebastion
 
 This enum stores the two item categories used by the inventory system.
 Primary items are used for scoring, while secondary items are used by the player.
+
+Bug: Arose when the player could softlock themselves by discarding a valuable prospect,
+making it so that they would never score enough to end the game.
+Fix: Ensure that all scoring prospects were classified differently from expendables to prevent
+players from softlocking themselves.
 */
 
 public enum ItemType {
